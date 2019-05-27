c44eb618c6ed1874b0bf66523919605b3df059f3
Natalia Suslova <suslova1991na@gmail.com>
2019-05-27 14:58:03 +0700

    treeviewnode code optimization

--- StaffTree/app/components/DBTreeViewNode.jsx
+++ StaffTree/app/components/DBTreeViewNode.jsx
@@ -4,32 +4,33 @@ class DBTreeViewNode extends React.Component {
         super(props);
     }
     render() {
-        if (this.props.item.subordinates && this.props.item.subordinates.length) {
             return (
                 <li>
-                    <h3><span class="treename">{this.props.item.name} </span> <button disabled={this.props.disabled} onClick={() => { this.props.addToCache(this.props.item)}} class="edit">ред.</button></h3>
+                    <h3>
+                        <span class="treename">{this.props.item.name} </span>
+                        <button
+                            disabled={this.props.disabled}
+                            onClick={() => { this.props.addToCache(this.props.item) }}
+                            class="edit">ред.                               
+                        </button>
+                    </h3>
                     <h3><b>Должность: </b>{this.props.item.position}</h3>
                     <h3><b>Зарплата: </b>{this.props.item.value} р.</h3>
-                    <details open><summary>Подчиненные: </summary>
-                        <ul>
-                        {this.props.item.subordinates.map(i => (
-                                <DBTreeViewNode item={i} disabled={this.props.disabled} addToCache={this.props.addToCache} />
-                        ))}
-                    </ul> </details>
+                    {this.props.item.subordinates && this.props.item.subordinates.length ? (
+                        <details open>
+                            <summary>Подчиненные: </summary>
+                            <ul>
+                                {this.props.item.subordinates.map(i => (
+                                    <DBTreeViewNode item={i} disabled={this.props.disabled} addToCache={this.props.addToCache} />
+                                ))}
+                            </ul>
+                        </details>
+                    ) : (
+                            <h3>Подчиненные: нет </h3>
+                        )
+                    }
                 </li>
-            );
+            )
         }
-        else {
-            return (
-                <li>
-                    <h3><span class="treename">{this.props.item.name} </span><button onClick={() => { this.props.addToCache(this.props.item)}} class="edit">ред.</button></h3>
-                    <h3><b>Должность: </b>{this.props.item.position}</h3>
-                    <h3><b>Зарплата: </b>{this.props.item.value} р.</h3>
-                    <h3>Подчиненные: нет </h3>
-                </li>
-            );
-        };
-        
-    }
 }
 module.exports = DBTreeViewNode;
